f24eb81b7ea131f2d0abfeefc8eb6d3c4387e89a
RiveSunder <RiveSunder@protonmail.com>
2021-05-23 22:24:44 -0600

    add meta-learning agent HARLI, and update CARLA policy (make hallucinations work)

--- game_of_carle/agents.py
+++ game_of_carle/agents.py
@@ -39,6 +39,9 @@ class TrainAgent(nn.Module):
 
         self.initialize_policy()
 
+    def reset(self):
+        pass
+
     def initialize_policy(self):
 
         in_dim = self.observation_width * self.observation_height
@@ -69,12 +72,13 @@ class CARLA(TrainAgent):
         self.generation = 0
         self.episodes = 0
         self.max_episodes = 1
-        self.ca_steps = 2
+        self.ca_steps = 32
 
         self.agents = []
         self.fitness = []
 
         self.save_path = kwargs["save_path"] if "save_path" in kwargs.keys() else "" 
+        self.instances = kwargs["instances"] if "instances" in kwargs.keys() else 1 
 
     def initialize_policy(self):
 
@@ -93,33 +97,33 @@ class CARLA(TrainAgent):
 
         self.cellular_rules = nn.Sequential(nn.Conv2d(9, 32, 1, bias=False),\
                 nn.ReLU(),
-                nn.Conv2d(32, 9, 1, bias=True),
-                nn.Sigmoid())
-                
+                nn.Conv2d(32, 1, 1, bias=True),
+                nn.Sigmoid()).to(self.my_device)
 
         for param in self.cellular_rules.named_parameters():
-            param[1].requires_grad = True
+            param[1].requires_grad = False
 
             if "bias" in param[0]:
-                param[1].data.fill_(param[1][0].item() - 16)
+                param[1].data.fill_(param[1][0].item() - 0.025)
 
-        self.hallucinogen = torch.nn.parameter(torch.rand())
+        self.hallucinogen = torch.nn.Parameter(torch.rand(1)/10,\
+                requires_grad=False).to(self.my_device)
             
 
     def hallucinate(self, obs):
 
-        obs = obs + (1.0 * (torch.rand_like(obs)  > self.hallucinogen)).float()
+        obs = obs + (1.0 * (torch.rand_like(obs)  < self.hallucinogen)).float()
+        obs[obs>1.0] = 1.0
 
         return obs 
         
     def forward(self, obs):
         
-        if obs.sum() < 100:
-            obs = self.hallucinate(obs)
-
-        my_grid = self.perceive(obs)
+        obs = self.hallucinate(obs)
 
         for jj in range(self.ca_steps):
+
+            my_grid = self.perceive(obs)
 
             my_grid = self.cellular_rules(my_grid)
 
@@ -131,7 +135,6 @@ class CARLA(TrainAgent):
 
         action_probabilities = my_grid[:,0:1,off_x:-off_x,off_y:-off_y]
         
-        
         action = (action_probabilities > 0.5).float()
 
         return action
@@ -139,7 +142,7 @@ class CARLA(TrainAgent):
     def get_params(self):
         params = np.array([])
 
-        params = np.append(self.hallucinogen)
+        params = np.append(params, self.hallucinogen.cpu().numpy())
 
         for param in self.cellular_rules.named_parameters():
             params = np.append(params, param[1].detach().cpu().numpy().ravel())
@@ -150,7 +153,8 @@ class CARLA(TrainAgent):
 
         param_start = 0
 
-        self.hallucinogen = my_params[0]
+        self.hallucinogen = nn.Parameter(torch.Tensor(my_params[0:1]), \
+                requires_grad=False).to(self.my_device)
 
         param_start += 1
 
@@ -168,8 +172,231 @@ class CARLA(TrainAgent):
 
 class HARLI(CARLA):
     """
-    Hebbian Automata Reinforcement Learning Intender
+    Hebbian Automata Reinforcement Learning Improviser
     """
+
+    def __init__(self, **kwargs):
+        super(HARLI, self).__init__(**kwargs)
+
+    def reset(self):
+
+        self.ca_0 = nn.Conv2d(9,32, 1, bias=False).to(self.my_device)
+        self.ca_1 = nn.Conv2d(32,1, 1, bias=False).to(self.my_device)
+
+        for param in self.ca_0.named_parameters():
+            param[1].requires_grad = False
+
+        for param in self.ca_1.named_parameters():
+            param[1].requires_grad = False
+
+
+        self.zero_eligibility()
+
+    def zero_eligibility(self):
+
+        self.oi_oj_0 = torch.zeros(32, 9).to(self.my_device)
+        self.oi_0 = torch.zeros(1, 9).to(self.my_device)
+        self.oj_0 = torch.zeros(32, 1).to(self.my_device)
+
+        self.oi_oj_1 = torch.zeros(1, 32).to(self.my_device)
+        self.oi_1 = torch.zeros(1, 32).to(self.my_device)
+        self.oj_1 = torch.zeros(1, 1).to(self.my_device)
+
+    def initialize_policy(self):
+        # initialize hebbian learning parameters
+
+        self.perceive = nn.Conv2d(1, 9, 3, groups=1, padding=1, stride=1, \
+                padding_mode="circular", bias=False)
+
+        self.perceive.weight.requires_grad = False
+
+        self.perceive.weight.data.fill_(0)
+
+        for ii in range(9):
+            self.perceive.weight[ii, :, ii//3, ii%3].fill_(1)
+
+        self.perceive.to(self.my_device)
+
+        self.eta_0 = nn.Parameter(torch.randn(32, 9) / 20.0, requires_grad=False).to(self.my_device)
+        self.a_0 = nn.Parameter(torch.randn(32, 9), requires_grad=False).to(self.my_device)
+        self.b_0 = nn.Parameter(torch.randn(1, 9), requires_grad=False).to(self.my_device)
+        self.c_0 = nn.Parameter(torch.randn(32, 1), requires_grad=False).to(self.my_device)
+        self.d_0 = nn.Parameter(torch.randn(32, 9), requires_grad=False).to(self.my_device)
+
+        self.eta_1 = nn.Parameter(torch.randn(1, 32) / 20.0, requires_grad=False).to(self.my_device)
+        self.a_1 = nn.Parameter(torch.randn(1, 32), requires_grad=False).to(self.my_device)
+        self.b_1 = nn.Parameter(torch.randn(1, 32), requires_grad=False).to(self.my_device)
+        self.c_1 = nn.Parameter(torch.randn(1, 1), requires_grad=False).to(self.my_device)
+        self.d_1 = nn.Parameter(torch.randn(1, 32), requires_grad=False).to(self.my_device)
+
+        self.act_0 = nn.Tanh()
+        self.act_1 = nn.Sigmoid()
+
+        self.reset()
+
+        self.hallucinogen = torch.nn.Parameter(torch.rand(1)/10,\
+                requires_grad=False).to(self.my_device)
+
+    def hebbian_update(self):
+        # update weights
+
+        for param_0 in self.ca_0.parameters():
+
+            param_0[:,:,0,0] += self.eta_0 * (\
+                    self.a_0 * self.oi_oj_0 \
+                    + self.b_0 * self.oi_0 \
+                    + self.c_0 * self.oj_0 \
+                    + self.d_0)
+
+        for param_1 in self.ca_1.parameters():
+
+            param_1[:,:,0,0] += self.eta_1 * (\
+                    self.a_1 * self.oi_oj_1 \
+                    + self.b_1 * self.oi_1 \
+                    + self.c_1 * self.oj_1 \
+                    + self.d_1)
+
+        if (0):
+            self.zero_eligibility()
+
+    def forward(self, obs):
+        
+        my_grid = self.hallucinate(obs)
+
+        for jj in range(self.ca_steps):
+
+            # eligibility traces calculated from pre/post synaptic
+            # values after activation function is applied, but could 
+            # just as easily use values from before activation
+
+            # calculate non-deterministic neighborhoods
+            my_grid_00 = self.perceive(my_grid)
+
+            # first layer of ca policy rules
+            my_grid_01 = self.ca_0(my_grid_00)
+            my_grid_01 = self.act_0(my_grid_01)
+
+            # first layer eligibility traces 
+            grid_00_mean = my_grid_00.mean(-1).mean(-1)
+            grid_01_mean = my_grid_01.mean(-1).mean(-1)
+
+            self.oi_0 += grid_00_mean.mean(0).unsqueeze(0)
+            self.oi_oj_0 += torch.matmul(grid_01_mean.T, grid_00_mean)
+            self.oj_0 += grid_01_mean.mean(0).unsqueeze(-1)
+
+            # second layer of ca policy rules 
+            my_grid_11 = self.ca_1(my_grid_01)
+            my_grid_11 = self.act_1(my_grid_11-0.025)
+
+            # second layer eligibility traces
+            grid_11_mean = my_grid_11.mean(-1).mean(-1)
+
+            self.oi_1 += grid_01_mean.mean(0).unsqueeze(0)
+            self.oi_oj_1 += torch.matmul(grid_11_mean.T, grid_01_mean)
+            self.oj_1 += grid_11_mean.mean(0).unsqueeze(-1)
+
+            my_grid = my_grid_11
+
+            #alive_mask = (my_grid[:,3:4,:,:] > 0.05).float()
+            #my_grid *= alive_mask
+
+        off_x = (obs.shape[2] - 64) // 2
+        off_y = (obs.shape[3] - 64) // 2
+
+        action_probabilities = my_grid[:,0:1,off_x:-off_x,off_y:-off_y]
+        
+        action = (action_probabilities > 0.5).float()
+
+        self.hebbian_update()
+
+        return action
+
+    def get_params(self):
+        params = np.array([])
+
+        params = np.append(params, self.hallucinogen.cpu().numpy())
+
+        params = np.append(params, self.eta_0.detach().cpu().numpy().ravel())
+        params = np.append(params, self.a_0.detach().cpu().numpy().ravel())
+        params = np.append(params, self.b_0.detach().cpu().numpy().ravel())
+        params = np.append(params, self.c_0.detach().cpu().numpy().ravel())
+        params = np.append(params, self.d_0.detach().cpu().numpy().ravel())
+
+        params = np.append(params, self.eta_1.detach().cpu().numpy().ravel())
+        params = np.append(params, self.a_1.detach().cpu().numpy().ravel())
+        params = np.append(params, self.b_1.detach().cpu().numpy().ravel())
+        params = np.append(params, self.c_1.detach().cpu().numpy().ravel())
+        params = np.append(params, self.d_1.detach().cpu().numpy().ravel())
+
+        return params
+
+    def set_params(self, my_params):
+
+        param_start = 0
+
+        self.hallucinogen = nn.Parameter(torch.Tensor(my_params[0:1])).to(self.my_device)
+
+        param_start = 1
+        param_stop = param_start + reduce(lambda x,y: x*y, self.eta_0.shape)
+        self.eta_0 = nn.Parameter(torch.Tensor(my_params[param_start:param_stop])\
+                .reshape(self.eta_0.shape),\
+                requires_grad=False).to(self.my_device)
+        param_start = param_stop
+
+        param_stop = param_start + reduce(lambda x,y: x*y, self.a_0.shape)
+        self.a_0 = nn.Parameter(torch.Tensor(my_params[param_start:param_stop])\
+                .reshape(self.a_0.shape),\
+                requires_grad=False).to(self.my_device)
+        param_start = param_stop
+
+        param_stop = param_start + reduce(lambda x,y: x*y, self.b_0.shape)
+        self.b_0 = nn.Parameter(torch.Tensor(my_params[param_start:param_stop])\
+                .reshape(self.b_0.shape),\
+                requires_grad=False).to(self.my_device)
+        param_start = param_stop
+
+        param_stop = param_start + reduce(lambda x,y: x*y, self.c_0.shape)
+        self.c_0 = nn.Parameter(torch.Tensor(my_params[param_start:param_stop])\
+                .reshape(self.c_0.shape),\
+                requires_grad=False).to(self.my_device)
+        param_start = param_stop
+
+        param_stop = param_start + reduce(lambda x,y: x*y, self.d_0.shape)
+        self.d_0 = nn.Parameter(torch.Tensor(my_params[param_start:param_stop])\
+                .reshape(self.d_0.shape),\
+                requires_grad=False).to(self.my_device)
+        param_start = param_stop
+
+        param_stop = param_start + reduce(lambda x,y: x*y, self.eta_1.shape)
+        self.eta_1 = nn.Parameter(torch.Tensor(my_params[param_start:param_stop])\
+                .reshape(self.eta_1.shape),\
+                requires_grad=False).to(self.my_device)
+        param_start = param_stop
+
+        param_stop = param_start + reduce(lambda x,y: x*y, self.a_1.shape)
+        self.a_1 = nn.Parameter(torch.Tensor(my_params[param_start:param_stop])\
+                .reshape(self.a_1.shape),\
+                requires_grad=False).to(self.my_device)
+        param_start = param_stop
+
+        param_stop = param_start + reduce(lambda x,y: x*y, self.b_1.shape)
+        self.b_1 = nn.Parameter(torch.Tensor(my_params[param_start:param_stop])\
+                .reshape(self.b_1.shape),\
+                requires_grad=False).to(self.my_device)
+        param_start = param_stop
+
+        param_stop = param_start + reduce(lambda x,y: x*y, self.c_1.shape)
+        self.c_1 = nn.Parameter(torch.Tensor(my_params[param_start:param_stop])\
+                .reshape(self.c_1.shape),\
+                requires_grad=False).to(self.my_device)
+        param_start = param_stop
+
+        param_stop = param_start + reduce(lambda x,y: x*y, self.d_1.shape)
+        self.d_1 = nn.Parameter(torch.Tensor(my_params[param_start:param_stop])\
+                .reshape(self.d_1.shape),\
+                requires_grad=False).to(self.my_device)
+        param_start = param_stop
+
 
 class ConvGRNNAgent(TrainAgent):
     def __init__(self, **kwargs):
@@ -395,111 +622,49 @@ class ConvGRNNAgent(TrainAgent):
             self.episodes += 1
 
 
-class CMAPopulation():
-    def __init__(self, agent_fn, device="cpu"):
-        """
-        """
-        self.use_grad = False
-        self.population_size = 16
-        self.meta_index = 0
-        self.generation = 0
-        self.episodes = 0
-        self.device = torch.device(device)
-
-        self.agents = []
-        self.fitness = []
-        
-        self.population = [agent_fn() for ii in range(self.population_size)]
-
-        for kk in range(self.population_size):
-            self.population[kk].to(self.device)
-
-        params = self.population[0].get_params()[np.newaxis,:]
-
-        for jj in range(1,len(self.population)):
-            params = np.append(params, \
-                    self.population[jj].get_params()[np.newaxis,:], axis=0)
-
-        means = np.mean(params, axis=0)
-        var = np.var(params, axis=0)
-
-        self.distribution = [means, np.diag(var)] 
-
-    def __call__(self, obs, agent_index=0):
-
-        action = self.population[agent_index + self.meta_index](obs)
-
-        return action
-
-    def update(self):
-        """
-        select champions and update the population distribution according to fitness
-
-        """
-        print("updating policy distribution")
-
-        self.agents = [agent.get_params() for agent in self.population] 
-
-        sorted_indices = list(np.argsort(np.array(self.fitness)))
-
-        sorted_indices.reverse()
-
-        sorted_params = np.array(self.agents)[sorted_indices]
-
-        keep = self.population_size // 4
-
-        elite_means = np.mean(sorted_params[0:keep], axis=0, keepdims=True)
-
-        covariance = np.matmul(\
-                (elite_means - self.distribution[0]).T,
-                (elite_means - self.distribution[0]))
-
-        self.distribution = [elite_means.squeeze(), covariance]
-
-
-        np.save("../policies/grnn_mean_gen{}.npy".format(self.generation), \
-                self.distribution[0])
-        np.save("../policies/grnn_covar_gen{}.npy".format(self.generation), \
-                self.distribution[1])
-
-        np.save("../policies/grnn_best_gen{}.npy".format(self.generation),\
-                self.agents[0])
-
-        self.agents = []
-        self.fitness = [] 
-
-        print("gen. {} updated policy distribution".format(self.generation))
-        self.generation += 1
-
-    def step(self, rewards=[0,0,0,0.]):
-        """
-        update agent(s)
-        this method is called everytime the CA universe is reset. 
-        """ 
-
-        self.fitness.extend(rewards)
-
-        if len(self.fitness) >= self.population_size :
-            self.update()
-        else:
-            self.meta_index = len(self.fitness)
-
 
 if __name__ == "__main__":
 
-    agent = CARLA()
 
+    env = CARLE(instances=4, use_cuda=True)
 
-    
+    agent = CARLA(device="cuda")
 
-    env = CARLE()
     obs = env.reset()
 
-    obs  += (torch.rand(1,1,256,256) > 0.95).float()
+    params = agent.get_params()
 
+    agent.set_params(params)
 
-    for kk in range(8):
+    t2 = time.time()
+    for kk in range(100):
         action = agent(obs)
 
-        print(action.sum())
+        #print(action.sum())
         obs, r, d, i = env.step(action)
+    
+    t3 = time.time()
+    
+   
+
+    agent = HARLI(device="cuda")
+
+    obs = env.reset()
+
+    params = agent.get_params()
+
+    agent.set_params(params)
+
+    t0 = time.time()
+    for kk in range(100):
+        action = agent(obs)
+
+        #print(action.sum())
+        obs, r, d, i = env.step(action)
+
+    t1 = time.time()
+
+    print(t1-t0, t3-t2)
+    import pdb; pdb.set_trace()
+    
+    
